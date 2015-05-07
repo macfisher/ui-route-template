1b1812cf47376d70401ed7b84df809d927a10f36
Mac Fisher <macfisher01@gmail.com>
2015-05-07 16:27:26 -0600

    setup views and states

--- app.js
+++ app.js
@@ -13,6 +13,21 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
 			templateUrl: 'partial-home.html'
 		})
 		
+		// nested list with custom controller
+		.state('home.list', {
+			url: '/list',
+			templateUrl: 'partial-home-list.html',
+			controller: function($scope) {
+				$scope.robots = ['augmentor', 'security', 'repair'];
+			}
+		})
+		
+		// nested list with random string data
+		.state('home.paragraph', {
+			url: '/paragraph',
+			template: 'Robots... everywhere...'
+		})
+		
 		// about page and multiple named views
 		.state('about', {
 			// come back to this...
